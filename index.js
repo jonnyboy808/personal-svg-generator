1d6212f17bbc813f3490c5615a4798cb72b1ccac
jonnyboy808 <125600257+jonnyboy808@users.noreply.github.com>
2023-04-24 09:12:14 -0700

    created questions const

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ const circle = require('./lib/circle');
 const square = require('./lib/square');
 const triangle = require('./lib/triangle');
 
+
+const questions = [
+    {
+        name: 'shape',
+        message: 'Choose a shape for your logo.',
+        type: 'list',
+        choices: ['circle, square, triangle'],
+    },
+]
