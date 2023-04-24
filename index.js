826a24c5f16ad1cf9160339de51edf127f7f4479
jonnyboy808 <125600257+jonnyboy808@users.noreply.github.com>
2023-04-23 23:08:00 -0700

    created const for shapes, fs, and inquirer

---
+++ index.js
@@ -0,0 +1,6 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const circle = require('./lib/circle');
+const square = require('./lib/square');
+const triangle = require('./lib/triangle');
+
